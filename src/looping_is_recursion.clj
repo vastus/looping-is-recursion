d2f58e789563e576be2fae0814dcf422a86f3194
juho H <juhoth@ovi.com>
2013-10-24 21:38:52 +0300

    done with hoop-a-loop

--- src/looping_is_recursion.clj
+++ src/looping_is_recursion.clj
@@ -16,33 +16,14 @@
                   :else (recur (rest s))))]
     (lasti a-seq)))
 
-(defn seq= [seq1 seq2]
-  (let [f (fn [a b]
-            (cond
-              (and (empty? a) (empty? b)) true
-              (or (empty? a) (empty? b)) false
-              (== (first a) (first b)) (recur (rest a) (rest b))
-              :else false))]
-    (f seq1 seq2)))
-
-;; TODO: Unable to resolve symbol: ...
-;; (defn seq= [a-seq] [b-seq]
-;;   (loop [a a-seq
-;;          b b-seq]
-;;     (cond
-;;       (and (empty? a) (empty? b)) true
-;;       (or (empty? a) (empty? b)) false
-;;       (== (first a) (first b)) (recur (rest a) (rest b))
-;;       :else false)))
-
-;; With let
-;; (defn find-first-index [pred a-seq]
-;;   (let [f (fn [idx pred a-seq]
-;;             (cond
-;;               (empty? a-seq) nil
-;;               (pred (first a-seq)) idx
-;;               :else (recur (inc idx) pred (rest a-seq))))]
-;;     (f 0 pred a-seq)))
+(defn seq= [a-seq b-seq]
+  (loop [a a-seq
+         b b-seq]
+    (cond
+      (and (empty? a) (empty? b)) true
+      (or (empty? a) (empty? b)) false
+      (== (first a) (first b)) (recur (rest a) (rest b))
+      :else false)))
 
 (defn find-first-index [predicate a-seq]
   (loop [idx 0
@@ -77,26 +58,17 @@
       (empty? a) s
       :else (recur (rest a) (toggle s (first a))))))
 
-;; def fibo(n):
-;;     if n == 0: return 0
-;;     eka = 0
-;;     toka = 1
-;;     fibo = 1
-;;     for i in range(n-1):
-;;         fibo = eka + toka
-;;         eka = toka
-;;         toka = fibo
-;;     return fibo
-
 (defn fast-fibo [no]
-  (loop [n no
-         eka 0
-         toka 1
-         fibo 1]
+  (loop [n no a 0 b 1 fibo 1]
     (cond
       (== no 0) 0
-      (== n 0) fibo)))
+      (< n 2) fibo
+      :else (recur (dec n) b (+ a b) (+ a b)))))
 
 (defn cut-at-repetition [a-seq]
-  [":("])
+  (loop [a a-seq dist []]
+    (cond
+      (empty? a) dist
+      (some #(= % (first a)) dist) dist
+      :else (recur (rest a) (conj dist (first a))))))
 
